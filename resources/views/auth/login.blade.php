cb03620094419eb2cdcfeae9127ed085ab24c521
Robeir George <robier_samir@hotmail.co.uk>
2025-07-22 00:05:08 +0300

    Fix Login Styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link href="{{ asset('assets/home_page/css/style.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('assets/home_page/css/style.css') }}" rel="stylesheet"> --}}
 
     <title>{{ __('login') }} || {{ config('app.name') }}</title>
 
